d4bda77ab77bdc72da659442a2d6c9bb9c951a69
aashishsharma555 <95011831+aashishsharma555@users.noreply.github.com>
2023-04-12 10:47:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+const questions = [
+    {
+        question: "HTML files are saved by default with the extension?",
+        answers: [
+            { text: ".html", correct: true},
+            { text: ".h", correct: false},
+            { text: ".ht", correct: false},
+            { text: "None of the above", correct: false},
+        ]
+    },
+    {
+        question: "How to create an ordered list in HTML?",
+        answers: [
+            { text: "<ul>", correct: false},
+            { text: "<ol>", correct: true},
+            { text: "<href>", correct: false},
+            { text: "<b>", correct: false},
+        ]  
+    },
+    {
+        question: "What is the smallest header in HTML by default?",
+        answers: [
+            { text: "h1", correct: false},
+            { text: "h2", correct: false},
+            { text: "h6", correct: true},
+            { text: "h4", correct: false},
+        ]
+    },
+    {
+        question: "How many sizes of headers are available in HTML by default?",
+        answers: [
+            { text: "5", correct: false},
+            { text: "1", correct: false},
+            { text: "3", correct: false},
+            { text: "6", correct: true},
+        ]
+    },
+    {
+        question: "How to display preformatted text in HTML?",
+        answers: [
+            { text: "<p>", correct: false},
+            { text: "<pre>", correct: true},
+            { text: "<hr>", correct: false},
+            { text: "All of the above", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `you score ${score} out of ${questions.length} !`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+
+startQuiz();
